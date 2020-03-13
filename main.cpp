3fa1aff2a831e1e4a8c6beeb471f37c73f5be761
Thomas <thomas.carney@griffithuni.edu.au>
2020-03-13 10:16:29 +1000

    classes work better together. Added operator overloading for board class (for 2d array [][])

--- main.cpp
+++ main.cpp
@@ -2,13 +2,9 @@
 
 class Square{
 public:
-    void setX(int xc){
-        this->x = xc;
-    }
 
-    void setY(int yc){
-        this->y = yc;
-    }
+    Square(): queen{false} {};
+    explicit Square(bool queenc): queen{queenc} {};
 
     void placeQueen(){
         this->queen = true;
@@ -23,17 +19,17 @@ public:
     }
 
 private:
-    int x;
-    int y;
     bool queen;
 };
 
 class Board{
 public:
+
     int getSize(){
         return n;
     }
 
+    //prints current board state (for debugging and visualisation mostly).
     void printBoard(){
         std:: cout << "    ";
         for(int i = 0; i < n; i++){
@@ -44,7 +40,7 @@ public:
             for(int j = 0; j < n + 1; j++){
                 if(j == 0){
                     std::cout << i << " ";
-                } else if(board[i][j - 1].hasQueen() == true) {
+                } else if(square[i][j - 1].hasQueen()) {
                     std::cout << "  " << "Q" << "  ";
                 } else {
                     std::cout << "  .  ";
@@ -54,24 +50,42 @@ public:
         }
     }
 
+    //init n*n board of 2d Squares by
     Board(){
-        board = new Square*[n];
-
+        square = new Square*[n];
         for(int i = 0; i < n; i++){
-            board[i] = new Square[n];
+            square[i] = new Square[n];
         }
+    }
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                board[i][j].setX(i);
-                board[i][j].setY(j);
-            }
+    //this class is just for 2 dimensional [][] operator overload (2nd [])
+    class operatorClass{
+    public:
+        explicit operatorClass(Square *squares): squares(squares){}
+
+        Square& operator[](int index){
+            //index is the value in the send set of [];
+            // e.g. board[nums][index];
+            return squares[index];
+        }
+    private:
+        Square * squares;
+    };
+
+    //outside operator overload (first [])
+    operatorClass operator [](int num){
+        //num is value in the first set of [];
+        if (num > n || num < 0){
+            std::cout << "Index out of bounds." << std::endl;
+            return operatorClass(square[0]);
+        } else {
+            return operatorClass(square[num]);
         }
     }
 
 private:
     int n = 4;
-    Square **board;
+    Square **square;
 
 };
 
@@ -79,7 +93,11 @@ private:
 
 int main() {
     Board board;
+    board[1][2].placeQueen();
+    board.printBoard();
 
+    board[0][0].placeQueen();
+    board[1][2].takeQueen();
     board.printBoard();
     return 0;
 }
